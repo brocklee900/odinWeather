fa3b7daeda5eca10e59aa67ca3dd6919f3e55488
Vincent Tran <83623108+brocklee900@users.noreply.github.com>
2025-09-23 13:47:16 -0700

    Add button to fetch weather data.
    Currently only logs to console. User may put in their own
    input/location query. Throws an error with an error message if their
    input is invalid (400 bad request error)

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("weather");
+
+const key = "CV8ZEVKQT9AYL8HWNSFFGQCEX";
+
+async function getData(location) {
+    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`;
+    let error = document.querySelector(".error");
+    console.log(error);
+
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            error.textContent = "";
+            return response.json();
+        } else if (response.status == 400) {
+            error.textContent = "Bad Request. Please check your input";
+            throw new Error("400 (Bad Request)");
+        } else {
+            throw new Error("Response not ok");
+        }
+    } catch (error) {
+        console.log("Whoops. There was an error");
+        console.error(error);
+        return undefined;
+    } 
+}
+
+async function search(location) {
+    const data = await getData(location);
+    console.log(data);
+}
+
+document.querySelector("#search").addEventListener("click", async (e) => {
+    let location = document.querySelector("#location").value;
+    const data = await getData(location); //wait for the response.json promise to resolve
+    if (data != undefined) {
+        console.log(data.currentConditions);
+    }
+}); 
